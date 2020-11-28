104a492e847dbbbf8f38806698630906bccbff49
iimanaliakte <75168096+iimanaliakte@users.noreply.github.com>
2020-11-28 16:33:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
-let results1;
-let api = `https://pokeapi.co/api/v2/pokemon?limit=0&offset=0`;
-let pokemon_api;
-let div = document.createElement("div");
+function fetchPokedex(){
 
- fetch(api)
-    .then(data => {
-        return data.json();
-        
+    for(let i = 1; i<11; i++){
+
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    fetch(url)
+    .then(respon =>{
+       return respon.json();
     })
-    .then(d =>{
-        for(var i = 0; i<20; i++){
-            results1 = d.results;
-            console.log(results1[i].name);
+    .then(data =>{
+        let pokemon = {
+            Name:data.forms[0].name,
+            Ability:data.abilities[0].ability.name,
+            Type:data.types.map((type) => type.type.name).join(', '),
+            Weight:data.weight,
+            Img:data.sprites.front_default
+        };
 
-        pokemon_api = results1[i].url;
-        console.log(pokemon_api);   
+        let div = document.createElement('div');
 
-            fetch(pokemon_api)
-                .then(res =>{
-                    return res.json();
-                })
-                .then(dat =>{
-                    console.log(dat.type.name);
-                })
+        let img = document.createElement('img');
+        img.setAttribute("src",pokemon.Img);
+  img.setAttribute("width", "304");
+  img.setAttribute("height", "228");
+  img.setAttribute("alt", "The Pulpit Rock");
+            div.appendChild(img);
 
+       let name = document.createElement('p');
+       name.innerText = pokemon.Name.charAt(0).toUpperCase() + pokemon.Name.slice(1);
+       
+    div.appendChild(name);
 
+            let ability = document.createElement('p');
+            ability.innerText = "Ability: " +pokemon.Ability.charAt(0).toUpperCase() + pokemon.Ability.slice(1);
 
-   let paraa = document.createElement('p');
+            div.appendChild(ability);
 
-          // paraa.className
+    let type = document.createElement('p');
+    type.innerText =  "Type: " + pokemon.Type.charAt(0).toUpperCase() + pokemon.Type.slice(1);
 
-            paraa.innerText = results1[i].name;
-        document.body.appendChild(paraa);
+        div.appendChild(type);
 
-    }
+        let weight = document.createElement('p');
+        weight.innerText = "Weight: " + pokemon.Type.charAt(0).toUpperCase() + pokemon.Type.slice(1);
 
-    }
-    );
+                document.body.appendChild(div);
+
+        console.log(pokemon);
+    })
+}
+}
+
+fetchPokedex();
 
 
 
